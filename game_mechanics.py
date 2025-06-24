65d7e78eaa146821a1e9bb3124d0429f13cd8bb3
Linden <lindenpowell218@gmail.com>
2025-06-24 16:28:50 +0800

    fixed capitalize(), added player and computer in __init__ + moves in mechanics

--- game_mechanics.py
+++ game_mechanics.py
@@ -3,14 +3,22 @@ from score import Scores
 score = Scores()
 score.score_tracker()
 class Gameplay:
+    def __init__(self):
+        self.player = ""
+        self.computer = ""
     def mechanics(self):
+        rps_move = ["ROCK", "PAPER", "SCISSORS"]
         while score.player < score.winning and score.computer < score.winning:
             random_num = randint(0,2)
             print("SCORES:")
             print(f"|Player 1: {score.player} || Computer: {score.computer}|")
-            self.player = input("Pick your move, Player: R - Rock | P - Paper | S - Scissors | Q - Quit ==").capitalize
+            self.player = input("Pick your move, Player: R - Rock | P - Paper | S - Scissors | Q - Quit ==").capitalize()
             if self.player == "Q":
+                print("Game is stopping..")
                 break
+            if random_num == 0:
+                self.computer = "ROCK"
+
 
 
 
