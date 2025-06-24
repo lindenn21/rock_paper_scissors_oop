e49c546293a4f1d09395fc89705b4a50d6b5cc75
Linden <lindenpowell218@gmail.com>
2025-06-24 17:52:01 +0800

    Added player moves + adding of  when player or computer wins

--- game_mechanics.py
+++ game_mechanics.py
@@ -4,28 +4,61 @@ score = Scores()
 score.score_tracker()
 class Gameplay:
     def __init__(self):
-        self.player = ""
-        self.computer = ""
+        self.player_move = ""
+        self.computer_move = ""
+
     def mechanics(self):
-        rps_move = ["ROCK", "PAPER", "SCISSORS"]
+        rps_move = {"R": "ROCK","P": "PAPER","S": "SCISSORS"}
         while score.player < score.winning and score.computer < score.winning:
             random_num = randint(0,2)
             print("SCORES:")
             print(f"|Player 1: {score.player} || Computer: {score.computer}|")
-            self.player = input("Pick your move, Player: R - Rock | P - Paper | S - Scissors | Q - Quit ==").capitalize()
+            self.player_move = input("Pick your move, Player: R - Rock | P - Paper | S - Scissors | Q - Quit ==").upper()
 
-            if self.player == "Q":
+            if self.player_move == "Q":
                 print("Game is stopping..")
                 break
 
             if random_num == 0:
-                self.computer = "ROCK"
+                self.computer_move = "ROCK"
             elif random_num == 1:
-                self.computer = "PAPER"
+                self.computer_move = "PAPER"
             else:
-                self.computer = "SCISSORS"
+                self.computer_move = "SCISSORS"
 
-            print(f"Computer Picks: {self.computer}")
+            print(f"Computer Picks: {self.computer_move}")
+
+            self.play()
+
+    def play(self):
+        if self.player_move == self.computer_move:
+            print("It's a tie!")
+        elif self.player_move == "R":
+            if self.computer_move == "SCISSORS":
+                print("Player: ROCK | Computer = SCISSORS | PLAYER WINS!")
+                score.player += 1
+            else:
+                print("Player: ROCK | Computer = PAPER | COMPUTER WINS!")
+                score.computer += 1
+        elif self.player_move == "P":
+            if self.computer_move == "ROCK":
+                print("Player: PAPER | Computer = ROCK | PLAYER WINS!")
+                score.player += 1
+            else:
+                print("Player: PAPER | Computer = SCISSORS | COMPUTER WINS!")
+                score.computer_move += 1
+        elif self.player_move == "S":
+            if self.computer_move == "PAPER":
+                print("Player: SCISSORS | Computer = PAPER | PLAYER WINS!")
+                score.player += 1
+            else:
+                print("Player: SCISSORS | Computer = ROCK | COMPUTER WINS!")
+                score.computer += 1
+        else:
+            print("Invalid move!")
+
+
+
 
 
 
--- score.py
+++ score.py
@@ -1,6 +1,6 @@
 # Scores
 class Scores:
-    def score_tracker(self, winning=2, player=0, computer=0):
+    def score_tracker(self, winning=5, player=0, computer=0):
         self.winning = winning
         self.player = player
         self.computer = computer
