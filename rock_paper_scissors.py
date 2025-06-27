e4388d0ca2f4fa47933e3deee377b02c822ea110
Linden <lindenpowell218@gmail.com>
2025-06-27 22:51:04 +0800

    Option to exit the game or play.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,7 +1,14 @@
 from game_mechanics import Gameplay
 class StartGame(Gameplay):
-    def start_game(self):
+    def start_game(self, ):
         print("Welcome to RPS!!")
+        print("First to 5 wins the game, would you like to play?")
+
+        choice = input("Yes or No?: ")
+
+
+
+
 
 
 game = Gameplay()
