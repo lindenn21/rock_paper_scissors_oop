1bb6b4e673680f6d519472fe7aaaa17f8af8366c
Linden <lindenpowell218@gmail.com>
2025-06-27 23:01:41 +0800

    Improved the start up

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,16 +1,24 @@
 from game_mechanics import Gameplay
 class StartGame(Gameplay):
-    def start_game(self, ):
+    def start_game(self):
         print("Welcome to RPS!!")
         print("First to 5 wins the game, would you like to play?")
+        print("1. Play the game")
+        print("2. Nope! I'm leaving.")
+        try:
+            choice = input("Choose: ")
+            if choice == "1":
+                self.mechanics()
+            else:
+                print("Alright! Goodbye.")
 
-        choice = input("Yes or No?: ")
 
 
 
 
 
 
-game = Gameplay()
-game.mechanics()
-game.moves()
+
+
+
+
